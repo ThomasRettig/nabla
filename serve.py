aa59ba19eca73b2b21a823dcfc19c792ade4dd1b
Thomas Rettig <thomas.rettig.11@gmail.com>
2022-05-11 19:37:13 +0800

    Update serve.py

--- serve.py
+++ serve.py
@@ -4,13 +4,13 @@ import socketserver
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
-            self.path = 'specimen.html'
+            self.path = 'index.html'
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
 # Create an object of the above class
 handler_object = MyHttpRequestHandler
 
-PORT = 800
+PORT = 8000
 my_server = socketserver.TCPServer(("", PORT), handler_object)
 
 # Star the server
